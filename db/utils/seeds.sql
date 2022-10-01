41e78e069473f4626fa101bab09112c03c7febb4
bethanyshoun <bethanyshoun@gmail.com>
2022-10-01 12:03:28 -0400

    work on seeds files

--- db/utils/seeds.sql
+++ db/utils/seeds.sql
@@ -1,27 +1,41 @@
-const express = require('express');
-const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+INSERT INTO departments (name)
+    VALUES
+        ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+INSERT INTO roles (title, salary, departments_id)
+    VALUES
+        ("Sales Lead", 135000, 1),
+        ("Salesperson", 90000, 1),
+        ("Lead Engineer", 165000, 2),
+        ("Software Engineeer", 120000, 2),
+        ("Account Mananger", 165000, 3),
+        ("Accountant", 115000, 3),
+        ("Lead Attorney", 225000, 4),
+        ("Associate Attorney", 155000, 4);
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+    VALUES
+        ('Jill', 'Fraser', 1),
+        ('Jack', 'Smith', 2, 1),
+        ('Rob', 'Edwards', 2, 1),
+        ('Peter', 'Green', 2, 1),
+        ('Sissy', 'Pitts', 2, 1),
+        ('Sam', 'Johnson', 2, 1),
+        ('Antoinette', 'Capone', 2, 1),
+        ('Danielle', 'Jefferies', 3),
+        ('Paolo', 'Pasolini', 4, 8),
+        ('Heather', 'Williams', 4, 8),
+        ('Sandy', 'Powell', 4, 8),
+        ('Emily', 'Zola', 4, 8),
+        ('Samantha', 'Delaney', 5),
+        ('Tony', 'Duval', 6, 14),
+        ('Denise', 'Cooper', 6, 14),
+        ('Monica', 'Bennet', 6, 14),
+        ('Aurora', 'Beardsley', 7),
+        ('Tomas', 'Rivera', 8, 18),
+        ('Willa', 'Morris', 8, 18);
+      
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// Start server after DB connection
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
