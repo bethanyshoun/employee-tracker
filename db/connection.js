3f3aeb6ca8bde0b0135124509a5d6f8861afc6b9
bethanyshoun <bethanyshoun@gmail.com>
2022-10-01 21:02:31 -0400

    commiting, needs work in several files

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,8 @@ const mysql = require("mysql2");
 // Connect to database
 const db = mysql.createConnection({
     host: 'localhost',
+
+    port: 3006,
     // Your MySQL username,
     user: 'root',
     // Your MySQL password 
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express');
+// const express = require('express');
 const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
+const inquirer = require('inquirer');
+//const mysql = require('mysql');
+//const PORT = process.env.PORT || 3001;
+//const app = express();
 
 // //cTable
 // const cTable = require('console.table');
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// const apiRoutes = require('./routes/apiRoutes');
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+// // Use apiRoutes
+// app.use('/api', apiRoutes);
+
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
 
 // Start server after DB connection
 db.connect(err => {
@@ -28,3 +30,28 @@ db.connect(err => {
     console.log(`Server running on port ${PORT}`);
   });
 });
+
+
+function beginProgram() {
+    inquirer
+      .prompt({
+        name: "options",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department"
+        ]
+      })
+      .then(function(req, res) {
+        console.log("You have entered:");
+         //add functions to handle input for each category
+         //switch cases?? 
+       });
+ }
+      
